ecf33df0e11f06e2d2679b3293e89345e0765e25
jayaprakash-trika <jayaprakash.m@trikatechnologies.com>
2023-06-27 12:55:50 +0530

    put command

---
+++ CRUD-express/routes/putmethod.js
@@ -0,0 +1,20 @@
+var express = require('express');
+var router = express.Router();
+
+
+
+
+router.put('/', function (req, res) {
+    let name = req.query.name;
+    // name = "suryaprakash";
+    res.send(name)
+});
+
+
+
+
+
+module.exports = router;
+
+
+
