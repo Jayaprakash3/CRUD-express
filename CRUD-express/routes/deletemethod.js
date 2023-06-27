c3a349210a845466eee7bf91f9dadf7ece794822
jayaprakash-trika <jayaprakash.m@trikatechnologies.com>
2023-06-27 15:37:21 +0530

    swagger added for delete command

--- CRUD-express/routes/deletemethod.js
+++ CRUD-express/routes/deletemethod.js
@@ -1,5 +1,41 @@
 var express = require('express');
 var router = express.Router();
+/**
+ * @swagger
+ * /delete/:
+ *   delete:
+ *     summary: Delete Location
+ *     tags:
+ *       - Locations
+ *     description: Delete Location 
+ *     produces:
+ *       - application/json
+ *     parameters:
+ *       - name: Delete location
+ *         description: Delete location details
+ *         in: body
+ *         default : '{"city":"Coimbatore","state":"TamilNadu","country":"India"}'
+ *         schema:
+ *           $ref: '#/definitions/DeleteLocations'
+ *     responses:
+ *       200:
+ *         description: Successfully created
+ */
+
+/**
+ * @swagger
+ * definitions:
+ *   DeleteLocations:
+ *     properties:
+ *       city:
+ *         type: string
+ *       state:
+ *         type: string
+ *       country:
+ *         type: string    
+ */
+
+
 
 router.delete('/', function (req, res) {
     let name = req.query.name;
