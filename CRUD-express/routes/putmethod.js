7ad6f0bbe08ced162ebb206d02c91e6c0bbeebe7
jayaprakash-trika <jayaprakash.m@trikatechnologies.com>
2023-06-27 15:33:24 +0530

    swagger added for put commands

--- CRUD-express/routes/putmethod.js
+++ CRUD-express/routes/putmethod.js
@@ -2,7 +2,40 @@ var express = require('express');
 var router = express.Router();
 
 
+/**
+ * @swagger
+ * /put/:
+ *   put:
+ *     summary: Edit the location details
+ *     tags:
+ *       - Locations
+ *     description: Update location details
+ *     produces:
+ *       - application/json
+ *     parameters:
+ *       - name: Location Details
+ *         description: Update location details
+ *         in: body
+ *         default : '{"city":"Coimbatore","state":"TamilNadu","country":"India"}'
+ *         schema:
+ *           $ref: '#/definitions/UpdateLocations'
+ *     responses:
+ *       200:
+ *         description: Successfully created
+ */
 
+/**
+ * @swagger
+ * definitions:
+ *   UpdateLocations:
+ *     properties:
+ *       city:
+ *         type: string
+ *       state:
+ *         type: string
+ *       country:
+ *         type: string    
+ */
 
 router.put('/', function (req, res) {
     let name = req.query.name;
