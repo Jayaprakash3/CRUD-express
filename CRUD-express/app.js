763c5837c778072e2a3e17e57ea917d2d64f4edd
jayaprakash-trika <jayaprakash.m@trikatechnologies.com>
2023-06-27 15:17:35 +0530

    added swagger for get command

--- CRUD-express/app.js
+++ CRUD-express/app.js
@@ -3,6 +3,8 @@ var express = require('express');
 var path = require('path');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
+const swaggerJSDoc = require('swagger-jsdoc');
+
 
 var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
@@ -30,10 +32,38 @@ app.use('/post', postRouter);
 app.use('/put', putRouter);
 app.use('/delete', deleteRouter);
 
+
+let swaggerDefinition = {
+  info: {
+    title: 'Learning - API Swagger Definition',
+    version: '1.0.0',
+    description: 'Demonstration of Organization Employee Portal API with Swagger',
+  },
+  // host: config.project.url,
+  basePath: '/',
+};
+
+
+// Options for the swagger docs
+let options = {
+  swaggerDefinition: swaggerDefinition,
+  apis: ['./routes/*.js'],
+};
+
+// initialize swagger-jsdoc
+let swaggerSpec = swaggerJSDoc(options);
+
+// serve swagger
+app.get('/swagger.json', function (req, res) {
+  res.setHeader('Content-Type', 'application/json');
+  res.send(swaggerSpec);
+});
+
 // catch 404 and forward to error handler
 app.use(function (req, res, next) {
   next(createError(404));
 });
+
 
 // error handler
 app.use(function (err, req, res, next) {
--- CRUD-express/routes/getmethod.js
+++ CRUD-express/routes/getmethod.js
@@ -1,11 +1,27 @@
 var express = require('express');
 var router = express.Router();
 
-router.get('/', function (req, res) {
-    res.send("express is working good")
+
+/**
+ * @swagger
+ * /get/try:
+ *   get:
+ *     summary: Get all location
+ *     tags:
+ *       - Locations
+ *     description:  Get all Location Details
+ *     produces:
+ *       - application/json
+ *     responses:
+ *       200:
+ *         description: Successfully received all the Location Details.
+ */
+router.get('/try', async function (req, res) {
+    // res.send("express is working good")
+    var obj = { "name": "jayaprakash", "age": "44" }
+    // var response = await locationModel.find();
+    res.send(obj)
 });
 
 module.exports = router;
-
-
 
